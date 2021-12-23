811607a07c68a5db25d7c0983d090da29bdd316b
unknown <trengling1@gmail.com>
2021-12-23 08:55:08 +0700

    deploy my raract app to github pages

---
+++ src/Khu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Khu extends Component {
+    render() {
+        return (
+            <div>
+                <p>hiha</p>
+            </div>
+        );
+    }
+}
+
+export default Khu;
